3db99e1631999c3362e553994aac84990e78a57c
genghao <genghao13@huawei.com>
2023-08-24 19:41:02 +0800

    SVG绘制Rect组件与Image组件时的尺寸错误
    Signed-off-by: genghao <genghao13@huawei.com>

--- modules/svg/src/SkSVGImage.cpp
+++ modules/svg/src/SkSVGImage.cpp
@@ -24,6 +24,7 @@ bool SkSVGImage::parseAndSetAttribute(const char* n, const char* v) {
                    "preserveAspectRatio", n, v));
 }
 
+
 bool SkSVGImage::onPrepareToRender(SkSVGRenderContext* ctx) const {
     // Width or height of 0 disables rendering per spec:
     // https://www.w3.org/TR/SVG11/struct.html#ImageElement
