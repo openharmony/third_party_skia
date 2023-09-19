f61f120b5d0a89b84c8c6b9c96b3cbbba1d33a91
凯鑫 <liukaixin@huawei.com>
2023-09-19 06:46:36 +0000

    update tools/SkSharingProc.cpp.
    Signed-off-by: 凯鑫 <liukaixin@huawei.com>

--- tools/SkSharingProc.cpp
+++ tools/SkSharingProc.cpp
@@ -24,7 +24,7 @@ namespace {
     }
 }
 
-__attribute__((visibility("default"))) void SkSharingSerialContext::collectNonTextureImagesFromPicture(
+SK_API void SkSharingSerialContext::collectNonTextureImagesFromPicture(
     const SkPicture* pic, SkSharingSerialContext* sharingCtx) {
     SkSerialProcs tempProc;
     tempProc.fImageCtx = sharingCtx;
@@ -33,7 +33,7 @@ __attribute__((visibility("default"))) void SkSharingSerialContext::collectNonTe
     pic->serialize(&ns, &tempProc);
 }
 
-__attribute__((visibility("default"))) sk_sp<SkData> SkSharingSerialContext::serializeImage(SkImage* img, void* ctx) {
+SK_API sk_sp<SkData> SkSharingSerialContext::serializeImage(SkImage* img, void* ctx) {
     SkSharingSerialContext* context = reinterpret_cast<SkSharingSerialContext*>(ctx);
     uint32_t id = img->uniqueID(); // get this process's id for the image. these are not hashes.
     // find out if we have already serialized this, and if so, what its in-file id is.
@@ -53,7 +53,7 @@ __attribute__((visibility("default"))) sk_sp<SkData> SkSharingSerialContext::ser
     return SkData::MakeWithCopy(&fid, sizeof(fid));
 }
 
-__attribute__((visibility("default"))) sk_sp<SkImage> SkSharingDeserialContext::deserializeImage(
+SK_API sk_sp<SkImage> SkSharingDeserialContext::deserializeImage(
   const void* data, size_t length, void* ctx) {
     if (!data || !length || !ctx) {
         SkDebugf("SkSharingDeserialContext::deserializeImage arguments invalid %p %zu %p.\n",
