3e88cc78cbd58b541d1f0a6b989ee502fe000ca7
whe <wanghongen1@huawei.com>
2024-05-10 14:51:52 +0800

    add xeg
    Signed-off-by: whe <wanghongen1@huawei.com>
    Change-Id: I8b371ce0027d674644b71bb3d6d3bcc391b8f050

--- src/gpu/vk/GrVkCommandBuffer.cpp
+++ src/gpu/vk/GrVkCommandBuffer.cpp
@@ -553,6 +553,16 @@ void GrVkPrimaryCommandBuffer::executeCommands(const GrVkGpu* gpu,
     this->invalidateState();
 }
 
+#ifdef SKIA_USE_XEG
+static std::atomic<uint64_t> g_totalFrameLoad(0);
+
+extern "C" __attribute__((visibility("default"))) void get_frame_load(uint64_t* frame_load)
+{
+    *frame_load = g_totalFrameLoad;
+    g_totalFrameLoad.store(0);
+}
+#endif
+
 static bool submit_to_queue(GrVkGpu* gpu,
                             VkQueue queue,
                             VkFence fence,
@@ -585,6 +595,14 @@ static bool submit_to_queue(GrVkGpu* gpu,
     submitInfo.pSignalSemaphores = signalSemaphores;
     VkResult result;
     GR_VK_CALL_RESULT(gpu, result, QueueSubmit(queue, 1, &submitInfo, fence));
+    
+#ifdef SKIA_USE_XEG
+    uint64_t frameLoad = 0;
+    GR_VK_CALL(gpu->vkInterface(), HMS_XEG_GetPerFrameLoad(commandBufferCount, commandBuffers, &frameLoad));
+    SkDebugf("frameLoad %{public}u\n", frameLoad);
+    g_totalFrameLoad += frameLoad;
+#endif
+    
     return result == VK_SUCCESS;
 }
 
