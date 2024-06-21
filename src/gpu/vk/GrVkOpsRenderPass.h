297cab812d1170c70659368e9ed319327dbd2a27
g30058336 <guqiuhan@huawei.com>
2024-06-21 14:23:01 +0800

    addBlurTraceAndTextUpload
    Signed-off-by: g30058336 <guqiuhan@huawei.com>
    Change-Id: Ifdfbaf47bf790bbf16dd5825776fb7bf4383967d

--- src/gpu/vk/GrVkOpsRenderPass.h
+++ src/gpu/vk/GrVkOpsRenderPass.h
@@ -121,7 +121,7 @@ private:
 
     GrVkGpu*                                    fGpu;
 
-    const float DRAW_BLUR_IMAGE_TIME = 1;
+    const float DRAW_BLUR_IMAGE_TIME = 1.0;
 
 #ifdef SK_DEBUG
     // When we are actively recording into the GrVkOpsRenderPass we set this flag to true. This
