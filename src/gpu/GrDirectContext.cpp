60c5c4d5d93767373232561124bff2af3eea2e18
猫科龙 <chenjinglong1@huawei.com>
2024-07-17 09:20:46 +0000

    update src/gpu/GrDirectContext.cpp.
    Signed-off-by: 猫科龙 <chenjinglong1@huawei.com>

--- src/gpu/GrDirectContext.cpp
+++ src/gpu/GrDirectContext.cpp
@@ -376,7 +376,9 @@ void GrDirectContext::purgeUnlockedResourcesByPid(bool scratchResourcesOnly, con
     // The StrikeCache indirectly references typeface, and in order to dereference the typeface,
     // it is necessary to clear the StrikeCache when the application exits.
     fStrikeCache->freeAll();
+#ifdef SK_VULKAN
     GrVkImage::PurgeAllocatedTextureBetweenFrames();
+#endif
 }
 
 void GrDirectContext::purgeCacheBetweenFrames(bool scratchResourcesOnly,
