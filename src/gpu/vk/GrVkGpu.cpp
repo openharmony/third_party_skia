9d3df1437fdc53c1f736c2e127b8dfea8d919322
g30058336 <guqiuhan@huawei.com>
2024-11-05 17:19:24 +0800

    addSkiaTrace
    Signed-off-by: g30058336 <guqiuhan@huawei.com>

--- src/gpu/vk/GrVkGpu.cpp
+++ src/gpu/vk/GrVkGpu.cpp
@@ -918,6 +918,12 @@ static size_t fill_in_compressed_regions(SkTArray<VkBufferImageCopy>* regions,
     return bufferSize;
 }
 
+static int get_current_time() {
+    return static_cast<int>(
+        std::chrono::duration_cast<std::chrono::microseconds>(
+        std::chrono::steady_clock::now().time_since_epoch()).count());
+}
+
 bool GrVkGpu::uploadTexDataOptimal(GrVkImage* texImage,
                                    SkIRect rect,
                                    GrColorType dataColorType,
@@ -984,6 +990,9 @@ bool GrVkGpu::uploadTexDataOptimal(GrVkImage* texImage,
 
     int currentWidth = rect.width();
     int currentHeight = rect.height();
+#ifdef SKIA_OHOS_FOR_OHOS_TRACE
+    bool isTagEnabled = IsTagEnabled(HITRACE_TAG_GRAPHIC_AGP);
+#endif
     for (int currentMipLevel = 0; currentMipLevel < mipLevelCount; currentMipLevel++) {
         if (texelsShallowCopy[currentMipLevel].fPixels) {
             const size_t trimRowBytes = currentWidth * bpp;
@@ -992,8 +1001,25 @@ bool GrVkGpu::uploadTexDataOptimal(GrVkImage* texImage,
             // copy data into the buffer, skipping the trailing bytes
             char* dst = buffer + individualMipOffsets[currentMipLevel];
             const char* src = (const char*)texelsShallowCopy[currentMipLevel].fPixels;
+#ifdef SKIA_OHOS_FOR_OHOS_TRACE
+            int memStartTimestamp = 0;
+            int memEndTimestamp = 0;
+            if (UNLIKELY(isTagEnabled)) {
+                memStartTimestamp = get_current_time();
+            }
+#endif
             SkRectMemcpy(dst, trimRowBytes, src, rowBytes, trimRowBytes, currentHeight);
-
+#ifdef SKIA_OHOS_FOR_OHOS_TRACE
+            if (UNLIKELY(isTagEnabled)) {
+                memEndTimestamp = get_current_time();
+                int duration = memEndTimestamp - memStartTimestamp;
+                if (duration > TRACE_LIMIT_TIME) {
+                    HITRACE_OHOS_NAME_FMT_ALWAYS("uploadTexDataOptimal SkRectMemcpy: %zu Time: %d μs bpp = %zu "
+                        "width: %d height: %d",
+                        trimRowBytes * currentHeight, duration, bpp, currentWidth, currentHeight);
+                }
+            }
+#endif
             VkBufferImageCopy& region = regions.push_back();
             memset(&region, 0, sizeof(VkBufferImageCopy));
             region.bufferOffset = slice.fOffset + individualMipOffsets[currentMipLevel];
@@ -1021,12 +1047,29 @@ bool GrVkGpu::uploadTexDataOptimal(GrVkImage* texImage,
     // command buffer has a ref on the buffer. This avoids having to add and remove a ref for ever
     // upload in the frame.
     GrVkBuffer* vkBuffer = static_cast<GrVkBuffer*>(slice.fBuffer);
+#ifdef SKIA_OHOS_FOR_OHOS_TRACE
+            int copyStartTimestamp = 0;
+            int copyEndTimestamp = 0;
+            if (UNLIKELY(isTagEnabled)) {
+                copyStartTimestamp = get_current_time();
+            }
+#endif
     this->currentCommandBuffer()->copyBufferToImage(this,
                                                     vkBuffer->vkBuffer(),
                                                     texImage,
                                                     VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL,
                                                     regions.count(),
                                                     regions.begin());
+#ifdef SKIA_OHOS_FOR_OHOS_TRACE
+    if (UNLIKELY(isTagEnabled)) {
+        copyEndTimestamp = get_current_time();
+        int duration = copyEndTimestamp - copyStartTimestamp;
+        if (duration > TRACE_LIMIT_TIME) {
+            HITRACE_OHOS_NAME_FMT_ALWAYS("uploadTexDataOptimal copyBufferToImage Time: %d μs width: %d height: %d",
+                duration, rect.width(), rect.height());
+        }
+    }
+#endif
     return true;
 }
 
--- src/gpu/vk/GrVkGpu.h
+++ src/gpu/vk/GrVkGpu.h
@@ -449,6 +449,10 @@ private:
     std::unique_ptr<GrVkMemoryReclaimer>                    fMemoryReclaimer;
 
     using INHERITED = GrGpu;
+
+#ifdef SKIA_OHOS_FOR_OHOS_TRACE
+    const int TRACE_LIMIT_TIME = 500;
+#endif
 };
 
 #endif
