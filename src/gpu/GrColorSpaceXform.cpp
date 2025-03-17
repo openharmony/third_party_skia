f15156db7bb05588442199f15362334c44cb37f9
mcczzz <547511415@qq.com>
2025-03-17 06:41:53 +0000

    enable CM2.0
    Signed-off-by: mcczzz <547511415@qq.com>

--- src/gpu/GrColorSpaceXform.cpp
+++ src/gpu/GrColorSpaceXform.cpp
@@ -89,7 +89,7 @@ SkString GrColorSpaceXformEffect::getShaderDfxInfo() const
     return format;
 }
 
-__attribute__((always_inline)) void GrColorSpaceXformEffect::onAddToKey(const GrShaderCaps&, GrProcessorKeyBuilder* b) const {
+SK_ALWAYS_INLINE void GrColorSpaceXformEffect::onAddToKey(const GrShaderCaps&, GrProcessorKeyBuilder* b) const {
     b->add32(GrColorSpaceXform::XformKey(fColorXform.get()));
 }
 
