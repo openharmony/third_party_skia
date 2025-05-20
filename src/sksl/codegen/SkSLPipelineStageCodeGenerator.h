6a36e744f6a9297b5d0beed0317bd067f17f7c52
Song Youcheng <songyoucheng@huawei.com>
2025-05-20 16:53:53 +0800

    use highp on mix intrinsic
    Signed-off-by: Song Youcheng <songyoucheng@huawei.com>

--- src/sksl/codegen/SkSLPipelineStageCodeGenerator.h
+++ src/sksl/codegen/SkSLPipelineStageCodeGenerator.h
@@ -50,11 +50,11 @@ namespace PipelineStage {
      * - Each invocation of sample() triggers a call to 'sampleChild', which should return the full
      *   text of the call expression.
      */
-    void ConvertProgram(const Program& program,
-                        const char* sampleCoords,
-                        const char* inputColor,
-                        const char* destColor,
-                        Callbacks* callbacks);
+    SK_API void ConvertProgram(const Program& program,
+                               const char* sampleCoords,
+                               const char* inputColor,
+                               const char* destColor,
+                               Callbacks* callbacks);
 }  // namespace PipelineStage
 
 }  // namespace SkSL
