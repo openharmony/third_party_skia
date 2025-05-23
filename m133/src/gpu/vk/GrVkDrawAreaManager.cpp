1da0b0ca0d959cea467d8113e6cb834e6cf25df7
chx <liuchuanxiu@h-partners.com>
2025-05-23 17:53:06 +0800

    fix
    Signed-off-by: chx <liuchuanxiu@h-partners.com>
    Change-Id: I35e2c224e7a653b814457757c17a2a83394fdf15

--- m133/src/gpu/vk/GrVkDrawAreaManager.cpp
+++ m133/src/gpu/vk/GrVkDrawAreaManager.cpp
@@ -1,25 +1,34 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
- * Description: Draw area manager for vulkan api partial render extended function.
- * Create: 2023/11/27
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include "src/gpu/vk/GrVkDrawAreaManager.h"
-#include "src/image/SkSurface_Gpu.h"
-#include "src/gpu/BaseDevice.h"
-#include "src/gpu/GrRenderTargetProxy.h"
+#include "src/gpu/ganesh/Device.h"
+#include "src/gpu/ganesh/GrRenderTargetProxy.h"
+#include "src/gpu/ganesh/surface/SkSurface_Ganesh.h"
 
 void GrVkDrawAreaManager::bindDrawingArea(SkSurface* surface, const std::vector<SkIRect>& skIRects) {
     if (!surface) {
         return;
     }
 
-    auto gpuDevice = static_cast<SkSurface_Gpu*>(surface)->getDevice();
+    auto gpuDevice = static_cast<SkSurface_Ganesh*>(surface)->getDevice();
     if (!gpuDevice) {
         return;
     }
 
-    auto gpuDeviceProxy = gpuDevice->asGpuDevice()->targetProxy();
+    auto gpuDeviceProxy = gpuDevice->targetProxy();
     if (!gpuDeviceProxy) {
         return;
     }
@@ -44,12 +53,12 @@ void GrVkDrawAreaManager::clearSurface(SkSurface* surface) {
         return;
     }
 
-    auto gpuDevice = static_cast<SkSurface_Gpu*>(surface)->getDevice();
+    auto gpuDevice = static_cast<SkSurface_Ganesh*>(surface)->getDevice();
     if (!gpuDevice) {
         return;
     }
 
-    auto gpuDeviceProxy = gpuDevice->asGpuDevice()->targetProxy();
+    auto gpuDeviceProxy = gpuDevice->targetProxy();
     if (!gpuDeviceProxy) {
         return;
     }
--- m133/src/gpu/vk/GrVkDrawAreaManager.h
+++ m133/src/gpu/vk/GrVkDrawAreaManager.h
@@ -1,7 +1,16 @@
 /*
- * Copyright (c) Huawei Technologies Co., Ltd. 2023-2023. All rights reserved.
- * Description: Draw area manager for vulkan api partial render extended function.
- * Create: 2023/11/27
+ * Copyright (c) 2025 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #ifndef GrVkDrawAreaManager_DEFINED
@@ -9,7 +18,8 @@
 
 #include <map>
 #include "include/core/SkSurface.h"
-#include "include/private/SkMutex.h"
+#include "include/private/base/SkMutex.h"
+#include "src/gpu/ganesh/GrRenderTarget.h"
 
 class SK_API GrVkDrawAreaManager {
 public:
