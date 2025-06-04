1660b6f8f1cdc9618c98c4fd543d8ede155de89f
ZY_encoder <zhouyong10@huawei.com>
2025-06-04 17:06:30 +0800

    适配skparagraph编译
    Signed-off-by: ZY_encoder <zhouyong10@huawei.com>

--- m133/modules/skparagraph/src/OneLineShaper.cpp
+++ m133/modules/skparagraph/src/OneLineShaper.cpp
@@ -7,7 +7,7 @@
 #ifdef ENABLE_TEXT_ENHANCE
 #include "src/Run.h"
 #include "include/TextGlobalConfig.h"
-#include "utils/text_trace.h"
+#include "trace.h"
 #endif
 
 #include <algorithm>
--- m133/modules/skparagraph/src/ParagraphImpl.cpp
+++ m133/modules/skparagraph/src/ParagraphImpl.cpp
@@ -29,7 +29,7 @@
 #include <utility>
 
 #ifdef ENABLE_TEXT_ENHANCE
-#include "utils/text_trace.h"
+#include "trace.h"
 #include "log.h"
 #include "include/TextGlobalConfig.h"
 #include "modules/skparagraph/src/TextLineBaseImpl.h"
