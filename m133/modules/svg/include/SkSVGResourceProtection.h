a768cc05c68f9d87e4912749f9298bc6ed5e30ef
wyykak <wangyiyuan4@huawei.com>
2026-08-19 15:23:03 +0800

    Skia SVG: add resource limits interfaces
    Signed-off-by: wyykak <wangyiyuan4@huawei.com>
    Change-Id: I9dd9b6056dbf571d7d8b1c5639f993485bb2b70a

---
+++ m133/modules/svg/include/SkSVGResourceProtection.h
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2026 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef SkSVGResourceProtection_DEFINED
+#define SkSVGResourceProtection_DEFINED
+
+#include <csignal>
+
+#define SK_SVG_RESOURCE_PROTECTION_REPORT() \
+    do {                                    \
+        raise(42); /* 42 is signal no. */   \
+    } while (false)
+
+#endif  // SkSVGResourceProtection_DEFINED
